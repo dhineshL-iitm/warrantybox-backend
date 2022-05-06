104495887d6a8171dd7675968650b26c22a3006d
dhineshL <dhineshdl22@gmail.com>
2022-05-06 13:17:00 +0530

    Spring Project Initialisation

---
+++ src/main/java/com/iitmhackathon/warrantyboxbackend/WarrantyboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iitmhackathon.warrantyboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrantyboxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarrantyboxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iitmhackathon/warrantyboxbackend/WarrantyboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitmhackathon.warrantyboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarrantyboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
