1c60a67c1ab7ed5b9d6d39786134d02d817b328f
dhineshL <dhineshdl22@gmail.com>
2022-05-07 12:48:10 +0530

    Service and utility methods added

---
+++ src/main/java/com/iitmhackathon/warrantyboxbackend/util/Utility.java
@@ -0,0 +1,24 @@
+package com.iitmhackathon.warrantyboxbackend.util;
+
+import com.iitmhackathon.warrantyboxbackend.entity.ApplicationUser;
+import com.iitmhackathon.warrantyboxbackend.model.UserModel;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class Utility {
+    public static ApplicationUser createUserFromModel(UserModel userModel, PasswordEncoder passwordEncoder){
+
+        ApplicationUser applicationUser = new ApplicationUser();
+        applicationUser.setUsername(userModel.getUsername());
+
+        // Encoding password
+        applicationUser.setPassword(passwordEncoder.encode(userModel.getPassword()));
+
+        applicationUser.setEnabled(true);
+        applicationUser.setRoles("ROLE_USER");
+        applicationUser.setAccountNonExpired(true);
+        applicationUser.setAccountNonLocked(true);
+        applicationUser.setCredentialsNonExpired(true);
+
+        return applicationUser;
+    }
+}
