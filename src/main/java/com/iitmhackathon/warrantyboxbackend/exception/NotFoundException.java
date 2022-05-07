ad25bfed544c04966158fbb05426529356083cf0
dhineshL <dhineshdl22@gmail.com>
2022-05-07 12:48:45 +0530

    Exception Handling for rest apis

---
+++ src/main/java/com/iitmhackathon/warrantyboxbackend/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.iitmhackathon.warrantyboxbackend.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/iitmhackathon/warrantyboxbackend/model/UserModel.java
@@ -0,0 +1,11 @@
+package com.iitmhackathon.warrantyboxbackend.model;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String username;
+    private String password;
+
+}
