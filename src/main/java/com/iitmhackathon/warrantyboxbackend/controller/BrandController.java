0dc137bcf05f342c85750f11306dc9fe6f8889b0
dhineshL <dhineshdl22@gmail.com>
2022-05-08 08:22:40 +0530

    Brand apis integration

---
+++ src/main/java/com/iitmhackathon/warrantyboxbackend/controller/BrandController.java
@@ -0,0 +1,29 @@
+package com.iitmhackathon.warrantyboxbackend.controller;
+
+import com.iitmhackathon.warrantyboxbackend.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/brand")
+public class BrandController {
+    @Autowired
+    private ApplicationService applicationService;
+
+    @GetMapping("/gettickets")
+    public ResponseEntity<?> getTickets(Principal principal){
+
+        return new ResponseEntity<>(applicationService.getTicketsByBrand(principal),HttpStatus.OK);
+    }
+
+    @PutMapping("/resolveticket")
+    public ResponseEntity<?> resolveTicket(@RequestParam("invoiceNo") String invoice){
+        applicationService.resolveTicket(invoice);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/iitmhackathon/warrantyboxbackend/model/UserModel.java
+++ src/main/java/com/iitmhackathon/warrantyboxbackend/model/UserModel.java
@@ -7,5 +7,6 @@ public class UserModel {
 
     private String username;
     private String password;
+    private String type;
 
 }
